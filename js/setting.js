b21a59db58c799a3df48532c92872d03997cc301
shinya <pemomomo@gmail.com>
2014-02-06 12:56:46 +0000

    保存形式のjsonのフォーマット決定
    git-svn-id: http://133.242.136.24/repos/favorite_urls/trunk@3 a3c24cd5-18d8-477b-8d97-e53e6ea97b68

--- js/setting.js
+++ js/setting.js
@@ -1,13 +1,104 @@
 (function(){
+	/**
+	 * ×ボタンをおした時の挙動
+	 */
 	function closing(target){
 		target.children('.close').click(function(){
 			$(this).parent('.data-set').remove();
 		});
 	}
 
+	/**
+	 * 新しいボックスの生成
+	 */
+	function generate(target){
+		$('#generate').click(function(){
+			var gen = $('.model').clone();
+			gen.removeClass('hide model').show();
+			$('.list').append(gen);
+			closing(gen);
+			saving(gen);
+		});
+	}
+
+	/**
+	 * 入力されている文字列が有効か判定する
+	 */
+	function trim(target, urlFlag){
+		var str = target.val().replace(/(^[\s　]+)|([\s　]+$)/g, "");
+		target.val(str);
+		return str;
+	}
+
+	/**
+	 * 渡ってきたｊｓｏｎを保存する
+	 */
+	function jsonSave(data){
+		console.log(data);
+	}
+
+	/**
+	 * urlデータの保存
+	 */
+	function saving(target){
+		target.find('.saving').click(function(){
+			var sid = $(this).parent().children('[name=sid]');
+			var name = $(this).parent().children('[name=name]');
+			var url = $(this).parent().children('[name=url]');
+
+			sidStr = trim(sid);
+			nameStr = trim(name);
+			urlStr = trim(url);
+
+//			isUrl = urlStr.match(/^(https?|ftp)(:¥/¥/[-_.!~*¥'()a-zA-Z0-9;¥/?:¥@&=+¥$,%#]+)$/);
+			isUrl = urlStr.match(/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w-.\/?%&=]*)?/);
+
+
+			if(sidStr && nameStr && urlStr){
+				if(!isUrl){
+					popup("URLの形式がただしくありません", 'danger');
+				}else{
+					var result = {};
+					result[sidStr] = {
+						name: nameStr,
+						url: urlStr,
+					};
+				}
+			}else{
+				popup("パラメーターが正しく設定されていません", 'danger');
+			}
+
+			// データが正しければ保存を行う
+			if(result){
+				jsonSave( result );
+			}
+
+			//			console.log(sidStr, nameStr, urlStr);
+
+		});
+
+
+	}
+
+	function popup(str, type){
+
+		if(!type){
+			type = '.alert-warning';
+		}else{
+			type = '.alert-' + type;
+		}
+
+		var target = $(type);
+		target.text(str).fadeIn(500);
+		setTimeout(function(){
+			target.fadeOut(1000,function(){
+				$(this).text('');
+			});
+		},2000);
+	}
+
+
 	$(document).ready(function(){
-
-
 		// 保存処理
 		$('#save').click(function(){
 			var url = $('#choko').text();
@@ -25,17 +116,9 @@
 			}
 		});
 
-		$('#generate').click(function(){
-			var gen = $('.model').clone();
-			gen.removeClass('hide model').show();
-			$('.list').append(gen);
-			closing(gen);
-		});
+		// 生成処理
+		generate();
 
-
-		$('.saving').click(function(){
-
-		});
 	});
 })();
 
